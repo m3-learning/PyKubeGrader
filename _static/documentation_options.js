081776f47600fbad52b2d5beb885742c5e19a5ca
jagar2 <jagar2@users.noreply.github.com>
2024-12-27 22:27:38 -0500

    deploy: 8719b0a07823ff2fad1f0b4e396f5fbd742ae8f3

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.post1.dev1+g42a2e4a',
+    VERSION: '0.0.post1.dev1+g8719b0a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
