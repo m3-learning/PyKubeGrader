81be7051fccc5e9ba2f355563cac08ffef1872ca
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2025-01-21 15:12:16 -0500

    Format and lint with Ruff

--- src/pykubegrader/build/clean_folder.py
+++ src/pykubegrader/build/clean_folder.py
@@ -5,7 +5,6 @@ import sys
 
 
 class FolderCleaner:
-    
     def __init__(self, root_folder: str):
         """
         Initializes the FolderCleaner with the root folder to clean.
--- src/pykubegrader/widgets/question_processor.py
+++ src/pykubegrader/widgets/question_processor.py
@@ -1,4 +1,3 @@
-import re
 import panel as pn
 
 
