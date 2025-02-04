08a80fc04497c2a6365ab7cc585bd3dcddaeecf9
Xinqiao Zhang <xz498@drexel.edu>
2025-02-04 12:09:49 -0500

    debugging

--- src/pykubegrader/telemetry.py
+++ src/pykubegrader/telemetry.py
@@ -331,6 +331,8 @@ def fill_grades_df(new_weekly_grades, assignments, student_subs):
         subs = [ 
             sub for sub in student_subs if sub["assignment_type"] == assignment["assignment_type"] and sub["week_number"] == assignment["week_number"]
         ]
+        print(assignment)
+        print(subs)
         if len(subs) == 0:
             print(assignment['title'], 0, assignment['max_score'])
             continue
