e7ee11c05650a7355d2b8928e98fd58e2dfbc8bf
jagar2 <jca92@drexel.edu>
2025-02-09 16:27:45 -0500

    update with constructor working

--- src/pykubegrader/grade_reports/get_my_grades_testing.py
+++ src/pykubegrader/grade_reports/get_my_grades_testing.py
@@ -54,8 +54,8 @@ class GradeReport:
         self.verbose = verbose
         self.assignment_list = assignments_list
 
-        # self.assignments, self.student_subs = get_assignments_submissions()
-        # self.new_grades_df = setup_grades_df(self.assignments)
+        self.assignments, self.student_subs = get_assignments_submissions()
+        self.new_grades_df = self.setup_grades_df()
         # self.new_weekly_grades = fill_grades_df(
         #     self.new_grades_df, self.assignments, self.student_subs
         # )
@@ -64,64 +64,25 @@ class GradeReport:
         #     self.assignments, self.current_week, self.new_weekly_grades, self.weights
         # )
 
-    @property
-    def weights(self):
-        return self._weights
+    def get_weekly_assignments(self):
+        """Get all weekly assignments from the assignment list configuration"""
+        weekly_assignments = [
+            assignment for assignment in self.assignment_list if assignment.weekly
+        ]
+        return weekly_assignments
 
-    @weights.setter
-    def weights(self, new_weights):
-        if not isinstance(new_weights, dict):
-            raise ValueError("Weights must be a dictionary")
-        self._weights = new_weights
+    def get_num_weeks(self):
+        """Get the number of weeks in the course"""
+        max_week_number = max(item["week_number"] for item in self.assignments)
+        return max_week_number
 
     def setup_grades_df(self):
 
-        assignment_types = [
-            assignment for assignment in assignments_list if assignment.weekly
-        ]
-
-        assignment_types = list(set([a["assignment_type"] for a in self.assignments]))
-        inds = [f"week{i + 1}" for i in range(11)] + ["Running Avg"]
-        restruct_grades = {k: [0 for i in range(len(inds))] for k in assignment_types}
+        weekly_assignments = self.get_weekly_assignments()
+        max_week_number = self.get_num_weeks()
+        inds = [f"week{i + 1}" for i in range(max_week_number)] + ["Running Avg"]
+        restruct_grades = {k.name: [0 for i in range(len(inds))] for k in weekly_assignments}
         new_weekly_grades = pd.DataFrame(restruct_grades, dtype=float)
         new_weekly_grades["inds"] = inds
         new_weekly_grades.set_index("inds", inplace=True)
         self.week_grades_df = new_weekly_grades
-
-
-# def get_my_grades_testing(start_date="2025-01-06", verbose=True):
-#     """takes in json.
-#     reshapes columns into reading, lecture, practicequiz, quiz, lab, attendance, homework, exam, final.
-#     fills in 0 for missing assignments
-#     calculate running average of each category"""
-
-#     # set up new df format
-#     weights = {
-#         "homework": 0.15,
-#         "lab": 0.15,
-#         "lecture": 0.15,
-#         "quiz": 0.15,
-#         "readings": 0.15,
-#         # 'midterm':0.15, 'final':0.2
-#         "labattendance": 0.05,
-#         "practicequiz": 0.05,
-#     }
-
-#     assignments, student_subs = get_assignments_submissions()
-
-#     new_grades_df = setup_grades_df(assignments)
-
-#     new_weekly_grades = fill_grades_df(new_grades_df, assignments, student_subs)
-
-#     current_week = get_current_week(start_date)
-
-#     avg_grades_dict = get_average_weighted_grade(
-#         assignments, current_week, new_weekly_grades, weights
-#     )
-
-#     if verbose:
-#         max_key_length = max(len(k) for k in weights.keys())
-#         for k, v in avg_grades_dict.items():
-#             print(f"{k:<{max_key_length}}:\t {v:.2f}")
-
-#     return new_weekly_grades  # get rid of test and running avg columns
