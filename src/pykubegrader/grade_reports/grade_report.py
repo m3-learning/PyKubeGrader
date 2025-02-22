80e1467cc94cb47681d5d4b487d545a0554c69c7
jagar2 <jca92@drexel.edu>
2025-02-21 20:32:28 -0500

    update to call function

--- src/pykubegrader/grade_reports/grade_report.py
+++ src/pykubegrader/grade_reports/grade_report.py
@@ -69,7 +69,7 @@ class GradeReport:
 
     def highlight_df(self):
         # Apply the styling to df2 based on NaNs in df1
-        styled_df = self.weekly_grades_df_display.style.apply(self.highlight_nan)
+        styled_df = self.weekly_grades_df_display.style.apply(self.highlight_nan())
 
         # Display the styled DataFrame
         return styled_df
