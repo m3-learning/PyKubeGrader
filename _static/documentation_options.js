db9d9c40cc35a7dc996cbd1f0142ed2d517dd6da
zhangxinqiao314 <zhangxinqiao314@users.noreply.github.com>
2025-03-04 12:01:51 -0500

    deploy: 8d97b3f8b9849fd754eeef6edeeebf72b2edce55

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.post1.dev1+gfada3ca',
+    VERSION: '0.0.post1.dev1+g8d97b3f',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
