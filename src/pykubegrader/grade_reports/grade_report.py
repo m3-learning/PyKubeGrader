320ea1a8640043f4f22fb84f222026b4d5592ab3
Xinqiao Zhang <xz498@drexel.edu>
2025-03-04 13:15:44 -0500

    remove unused final average calculation method and clean up grading configuration

--- src/pykubegrader/grade_reports/grade_report.py
+++ src/pykubegrader/grade_reports/grade_report.py
@@ -132,27 +132,6 @@ class GradeReport:
         else:
             return styler
 
-    # def _calculate_final_average(self):
-    #     total_percentage = 1
-    #     df_ = self.compute_final_average()
-    #     score_earned = 0
-
-    #     for assignment_type in self.assignment_type_list:
-    #         if assignment_type.name in exclude_from_running_avg:
-    #             total_percentage -= assignment_type.weight
-
-    #         score_earned += assignment_type.weight * df_[assignment_type.name]
-
-    #     self.final_grade = score_earned / total_percentage
-    #     self.weighted_average_grades = pd.concat(
-    #         [
-    #             pd.DataFrame(self.final_grades),
-    #             pd.DataFrame(
-    #                 {"Running Avg": [self.final_grade]},
-    #                 index=["Weighted Average Grade"],
-    #             ),
-    #         ]
-    #     )
     def _calculate_final_average(self):
         total_percentage = 1
         df_ = self.compute_final_average()
@@ -180,8 +159,9 @@ class GradeReport:
             score = 0
             for i, assignment_type in enumerate(optional_weighted_assignments):
                 score += weight_combo[i]*df_[assignment_type.name]
+            
             final_scores_list.append(non_optional_score*(1-sum(weight_combo)) + score)
-        print(final_scores_list)        
+
         self.final_grade = score_earned / total_percentage
         self.final_grade_final = max(*final_scores_list)
         self.weighted_average_grades = pd.concat(
