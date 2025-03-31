5a10f1ea86223e3908facae6c9296310f153500c
jagar2 <jca92@drexel.edu>
2025-03-31 15:24:23 -0400

    update name to set_response_JSON

--- src/pykubegrader/logging/responses.py
+++ src/pykubegrader/logging/responses.py
@@ -24,7 +24,21 @@ def log_encrypted(logger: logging.Logger, message: str) -> None:
     logger.info(f"Encrypted Output: {encrypted_b64}")
 
 @block_direct_notebook_calls
-def log_variable(assignment_name, value, info_type) -> None:
+def log_variable(assignment_name: str, value: Any, info_type: str) -> None:
+    """
+    Logs a variable with its assignment name, value, and type, along with a timestamp.
+
+    This function constructs a log message containing the assignment name, the type of information,
+    the value, and a timestamp. The message is then encrypted and logged using the reduced logger.
+
+    Args:
+        assignment_name (str): The name of the assignment.
+        value (Any): The value to be logged.
+        info_type (str): The type of information being logged.
+
+    Returns:
+        None
+    """
     timestamp = datetime.datetime.now(datetime.timezone.utc).isoformat(
         sep=" ", timespec="seconds"
     )
@@ -33,6 +47,15 @@ def log_variable(assignment_name, value, info_type) -> None:
 
 
 def ensure_responses() -> dict[str, Any]:
+    """
+    Ensures the existence and integrity of the responses file.
+
+    This function checks for the existence of the .responses.json file and ensures it is properly formatted.
+    If the file does not exist or is not properly formatted, it initializes an empty dictionary and writes it to the file.
+
+    Returns:
+        dict[str, Any]: The responses dictionary loaded from the .responses.json file.
+    """
     with open(".responses.json", "a") as _:
         pass
 
@@ -48,7 +71,24 @@ def ensure_responses() -> dict[str, Any]:
     return responses
 
 
-def update_responses(key: str, value) -> dict:
+def set_response_json(key: str, value) -> dict:
+    """
+    Updates the responses dictionary with a new key-value pair and writes it to the .responses.json file.
+
+    This function ensures the existence and integrity of the responses file, updates it with the provided key-value pair,
+    and writes the updated dictionary back to the file. It uses a temporary file to ensure atomicity of the write operation.
+
+    Args:
+        key (str): The key to be added or updated in the responses dictionary.
+        value (Any): The value to be associated with the key in the responses dictionary.
+
+    Returns:
+        dict: The updated responses dictionary.
+
+    Raises:
+        TypeError: If the value provided is not serializable to JSON.
+        json.JSONDecodeError: If there is an error decoding the JSON file.
+    """
     data = ensure_responses()
     data[key] = value
 
