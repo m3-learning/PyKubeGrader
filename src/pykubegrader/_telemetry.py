44bcddac1b86e00dc7edb2fadf3990b7a8392069
jagar2 <jca92@drexel.edu>
2025-04-01 17:50:53 -0400

    logging config

--- src/pykubegrader/_telemetry.py
+++ src/pykubegrader/_telemetry.py
@@ -12,25 +12,7 @@ from requests.auth import HTTPBasicAuth
 from pykubegrader.telemetry.responses import log_encrypted
 from pykubegrader._utils import api_base_url, student_pw, student_user
 
-#
-# Logging setup
-#
 
-# Logger for cell execution
-logger_code = logging.getLogger("code_logger")
-logger_code.setLevel(logging.INFO)
-
-file_handler_code = logging.FileHandler(".output_code.log")
-file_handler_code.setLevel(logging.INFO)
-logger_code.addHandler(file_handler_code)
-
-# Logger for question scores etc.
-logger_reduced = logging.getLogger("reduced_logger")
-logger_reduced.setLevel(logging.INFO)
-
-file_handler_reduced = logging.FileHandler(".output_reduced.log")
-file_handler_reduced.setLevel(logging.INFO)
-logger_reduced.addHandler(file_handler_reduced)
 
 
 def telemetry(info: ExecutionInfo) -> None:
