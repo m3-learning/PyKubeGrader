f1aeec32084eaeaa00d248b5f176488898795393
jagar2 <jca92@drexel.edu>
2025-04-07 22:54:29 -0400

    update util docs

--- src/pykubegrader/build/util.py
+++ src/pykubegrader/build/util.py
@@ -28,7 +28,18 @@ def get_due_date(assignment):
 
 
 def json_serial(obj):
-    """JSON serializer for objects not serializable by default."""
+    """
+    JSON serializer for objects not serializable by default.
+
+    Args:
+        obj: The object to serialize.
+
+    Returns:
+        str: The ISO format string representation of the datetime object.
+
+    Raises:
+        TypeError: If the object is not serializable.
+    """
     if isinstance(obj, datetime):
         return obj.isoformat()
     raise TypeError(f"Type {type(obj)} not serializable")
@@ -36,18 +47,50 @@ def json_serial(obj):
 
 @dataclass
 class EncryptionKeyBaseClass:
+    """
+    A base class for handling encryption key paths and operations related to transferring these keys.
+
+    Attributes:
+        client_private_key_path (str): The file path to the client's private key.
+        server_public_key_path (str): The file path to the server's public key.
+    """
+
     client_private_key_path: str
     server_public_key_path: str
 
     @property
-    def client_private_key_filename(self):
+    def client_private_key_filename(self) -> str:
+        """
+        Extracts the filename from the client's private key path.
+
+        Returns:
+            str: The filename of the client's private key.
+        """
         return os.path.basename(self.client_private_key_path)
 
     @property
-    def server_public_key_filename(self):
+    def server_public_key_filename(self) -> str:
+        """
+        Extracts the filename from the server's public key path.
+
+        Returns:
+            str: The filename of the server's public key.
+        """
         return os.path.basename(self.server_public_key_path)
 
-    def transfer_encryption_keys(self, temp_notebook_path):
+    def transfer_encryption_keys(self, temp_notebook_path: str) -> tuple:
+        """
+        Copies the encryption keys to the directory of the specified temporary notebook path.
+
+        Args:
+            temp_notebook_path (str): The path to the temporary notebook where the keys will be transferred.
+
+        Returns:
+            tuple: A tuple containing the paths to the copied client private key and server public key.
+
+        Raises:
+            IOError: If the file copy operation fails.
+        """
         client_private_key = os.path.join(
             os.path.dirname(temp_notebook_path),
             self.client_private_key_filename,
